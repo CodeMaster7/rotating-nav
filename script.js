228b65228e10e0b8bd73d5de9c2959538bc3ebcc
sam <sammybonfanti@gmail.com>
2021-02-18 20:41:00 -0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict"
+const open = document.getElementById('open');
+console.log("🚀 ~ file: script.js ~ line 3 ~ open", open)
+const close = document.getElementById('close');
+console.log("🚀 ~ file: script.js ~ line 5 ~ close", close)
+
+const container = document.querySelector('.container')
+console.log("🚀 ~ file: script.js ~ line 7 ~ container", container)
+
+open.addEventListener("click", () => container.classList.add('show-nav'))
+close.addEventListener("click", () => container.classList.remove('show-nav'))
